9840e9acf45cb8e7e92e34bcf73c491a8a0d2a31
Benjamin Fildier <benjamin.fildier@gmail.com>
2018-08-27 19:44:41 -0700

    script to convert all simulations at once

--- process_outputs/convert2nc_all.sh
+++ process_outputs/convert2nc_all.sh
@@ -1,61 +1,50 @@
 #!/bin/bash
 
-machine=$1
+machine=coriknl
+
+simroot="RCE_MPDATAxTKExCAMxSAM1MOM_4000x4000x15_128x128x32"
+SGS_all='SMAG TKE'
+CS_all='0 005 01 015 02'
+Ns='1 2 3'
+
+currentsim=false
+doout2d=true
+doout3d=false
+dooutstat=true
 
 # Target directory where is stored the output
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Define UTILDIR and OUTPUTDIR
 . ${SCRIPTDIR}/../load_dirnames.sh ${machine}
-SCRIPTNAME=`basename "$0"`
 
-# What/where to convert
-currentsim=true # true if in model's output dir, false if in directory given in argument
-doout2d=false
-doout3d=false
-dooutstat=true
+# Replace parameters in convert2nc.sh
+convertscript=convert2nc.sh
+for option in currentsim doout2d doout3d dooutstat; do
+    echo Set $option to ${!option}
+    if [ "$machine" == "tornado" ]; then
+         sed -i '' "s/${option}=.*/${option}=${!option}/" ${SCRIPTDIR}/$convertscript
+    elif [ "$machine" == "coriknl" ]; then
+         sed -i "s/${option}=.*/${option}=${!option}/" ${SCRIPTDIR}/$convertscript
+    fi
+done
 
-echo $currentsim
-
-if [[ "$currentsim" == "true" ]]; then
-    TARGETDIR=${OUTPUTDIR}
-else
-    TARGETDIR=${ARCHIVEDIR}/${machine}/$2
-fi
-
-echo $OUTPUTDIR
-echo $TARGETDIR
-cd $TARGETDIR
-
-# In OUT_2D
-if [[ "$doout2d" == "true" ]]; then
-    for file in `ls OUT_2D/*.2Dcom`; do
-    	echo $file
-        ${UTILDIR}/2Dcom2nc ${file} >> ${TARGETDIR}/OUT_2D/${SCRIPTNAME}.log \
-        2>> ${TARGETDIR}/OUT_2D/${SCRIPTNAME}.err
+# Convert all simulations
+for SGS in `echo ${SGS_all}`; do
+  for CS in `echo ${CS_all}`; do
+    for N in `echo ${Ns}`; do
+      simname=${simroot}_${SGS}-CS${CS}-r${N}
+      if [ ! -d ${ARCHIVEDIR}/${machine}/$simname ]; then
+        echo "passing $simname, simulation doesn't exist"
+        continue
+      fi
+      # print header
+      perl -E 'say "-" x 75'
+#      printf "-   %-70s\n" "$simname"
+      echo "-   $simname"
+      perl -E 'say "-" x 75'
+      # execute conversion
+      ${SCRIPTDIR}/$convertscript $machine $simname
     done
-fi
+  done
+done
 
-# In OUT_3D
-if [[ "$doout3d" == "true" ]]; then
-	# If the simulation is 2D
-    for file in `ls OUT_3D/*.com2D`; do
-    	echo $file
-        ${UTILDIR}/com2D2nc ${file} >> ${TARGETDIR}/OUT_3D/${SCRIPTNAME}.log \
-        2>> ${TARGETDIR}/OUT_3D/${SCRIPTNAME}.err
-    done
-    # If the simulation is 3D
-    for file in `ls OUT_3D/*.com3D`; do
-    	echo $file
-        ${UTILDIR}/com3D2nc ${file} >> ${TARGETDIR}/OUT_3D/${SCRIPTNAME}.log \
-        2>> ${TARGETDIR}/OUT_3D/${SCRIPTNAME}.err
-    done
-fi
-
-# In OUT_STAT
-if [[ "$dooutstat" == "true" ]]; then
-    for file in `ls OUT_STAT/*.stat`; do
-    	echo $file
-        ${UTILDIR}/stat2nc ${file} #>> ${TARGETDIR}/OUT_STAT/${SCRIPTNAME}.log \
-        #2>> ${TARGETDIR}/OUT_STAT/${SCRIPTNAME}.err
-    done
-fi
